7f4a02c4b23abc2bfb4adf3676962210d2eba489
Hset Paing <oonphyo93@gmail.com>
2018-07-23 17:11:58 +0630

    LoginBusinessMethod

--- src/main/java/com/contact/service/UserServiceImpl.java
+++ src/main/java/com/contact/service/UserServiceImpl.java
@@ -1,24 +1,26 @@
 package com.contact.service;
 
+import java.util.HashMap;
 import java.util.List;
-
-import javax.transaction.Transactional;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.contact.dao.BaseDAO;
 import com.contact.dao.UserDAO;
 import com.contact.domain.User;
 import com.contact.execption.UserBlockedException;
- 
+import com.contact.rm.UserRowMapper;
+
 /**
  * @author HsetPaing
  *
  */
 @Service
-public class UserServiceImpl extends BaseDAO implements UserService{
-	
+public class UserServiceImpl extends BaseDAO implements UserService {
+
 	@Autowired
 	private UserDAO userDAO;
 
@@ -26,13 +28,31 @@ public class UserServiceImpl extends BaseDAO implements UserService{
 	public void regiseter(User u) {
 		// TODO Auto-generated method stub
 		userDAO.save(u);
-		
+
 	}
 
 	@Override
 	public User login(String loginName, String password) throws UserBlockedException {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT userId,name,phone,email,address,role,loginStatus,loginName"
+				+ "FROM user WHERE loginName=:ln AND password=:pw";
+
+		Map m = new HashMap();
+		m.put("ln", loginName);
+		m.put("pw", password);
+		try {
+			User user = getNamedParameterJdbcTemplate().queryForObject(sql, m, new UserRowMapper());
+			if (user.getLoginStatus() == UserService.LOGIN_STATUS_ACTIVE) {
+				throw new UserBlockedException("Your accout has been blocked. Contact to admin.");
+			} else {
+				return user;
+			}
+
+		} catch (EmptyResultDataAccessException ex) {
+			// TODO: handle exception
+			return null;
+		}
+
 	}
 
 	@Override
@@ -44,7 +64,7 @@ public class UserServiceImpl extends BaseDAO implements UserService{
 	@Override
 	public void changeLoginStatus(int userId, int loginStatus) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
